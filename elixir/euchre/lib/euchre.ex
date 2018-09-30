e574ebb4ac5edfd81c4698e2f8d2b26cc8973b80
karl <karl.statz@gmail.com>
2018-09-29 23:17:03 -0500

    initial elixir project scaffold

---
+++ elixir/euchre/lib/euchre.ex
@@ -0,0 +1,18 @@
+defmodule Euchre do
+  @moduledoc """
+  Documentation for Euchre.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Euchre.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/euchre/test/euchre_test.exs
@@ -0,0 +1,8 @@
+defmodule EuchreTest do
+  use ExUnit.Case
+  doctest Euchre
+
+  test "greets the world" do
+    assert Euchre.hello() == :world
+  end
+end
